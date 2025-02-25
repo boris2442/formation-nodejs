c4f83d48e5491624dbb9a3e10cd00e957db99a98
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-25 11:53:56 +0100

    text

---
+++ module5/server-module.js
@@ -0,0 +1,9 @@
+// const http=require('http');
+import http from 'http'; //
+const server=http.createServer((req, res)=>{
+    res.writeHead(200, {'content-Type':'text-plain'});
+    res.end("Bonjour, bienvenue sur mon serveur nodejs");
+})
+server.listen(3000, ()=>{
+    console.log('Serveur démarré sur http://localhost:3000');
+})
