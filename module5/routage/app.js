807e472928cc86b11dec92fee80153b332962061
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-25 12:47:00 +0100

    text

--- module5/routage/app.js
+++ module5/routage/app.js
@@ -1,12 +1,29 @@
 //ici, nous devons parcourir l'url du navigateurs
 
+//ici, il s'agit du type module et la configuration se fait dans le terminal
 
 // const http=require('http');
-import http from 'http'; //
-const server=http.createServer((req, res)=>{
-    res.writeHead(200, {'content-Type':'text-plain'});
-    res.end("Bonjour, bienvenue sur mon serveur nodejs");
-})
-server.listen(3001, ()=>{
-    console.log('Serveur démarré sur http://localhost:3001');
-})
+import http from "http"; //
+import fileSystem from "fs"; //
+import path from "path";
+const server = http.createServer((req, res) => {
+  console.log(`${req.url}, ${req.method}`);
+  //gestion des rooutes bases sur l'url demande
+  if (req.url === "/" || req.url === "/home") {
+    res.writeHead(200, { "content-Type": "text/plain; charset=utf-8" });
+    res.end("Page d accueil du site");
+    // })
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-Type": "text/plain; charset=utf-8" });
+    res.end("Bienvenue dans note section a propos");
+  } else {
+    res.writeHead(404, { "content-type": "text/html; charset=utf-8" });
+    res.end("<h1 style='color:red;'>Page non trouvée</h1>");
+  }
+
+  res.writeHead(200, { "content-Type": "text/plain" });
+  res.end("Bonjour, bienvenue sur mon serveur nodejs");
+});
+server.listen(3003, () => {
+  console.log("Serveur démarré sur http://localhost:3003");
+});
