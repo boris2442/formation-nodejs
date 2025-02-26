ca9bde8b7f26371f7d893f19595ee8da2eb7183d
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-26 14:29:21 +0100

    text

--- module6/backend/server.js
+++ module6/backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
-const port=3301;
-const app=new express();
+const port=3001;
+const app= express();
+
+app.get('/post', (req, res)=>{
+    res.end('Welcome to you');
+})
 app.listen(port, ()=>{
     console.log(`serveur creer sur le port ${port} avec succes`);
 })
