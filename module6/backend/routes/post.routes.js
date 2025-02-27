b99ba00c96a8d7e8f218885a89eda9d6662f107f
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-27 08:12:35 +0100

    text

---
+++ module6/backend/routes/post.routes.js
@@ -0,0 +1,22 @@
+// const express=require('express');
+import express from 'express'
+const router=express.Router();
+//permet de creer les differentes routes!
+
+router.get('/', (req, res)=>{
+    res.json({
+        message:'home page'
+    })
+})
+router.get('/post', (req, res)=>{
+    res.json({
+        id:1,
+        title:'Mon premier post',
+        content:'contenu du post'
+    })
+})
+
+
+//dans le cas ou c'est common js module.exports=router
+//export default router dans  le cas de ecmascript 
+export default router
--- module6/backend/server.js
+++ module6/backend/server.js
@@ -1,10 +1,34 @@
 import express from 'express';
+//create a router variable 
+import postRoute from './routes/post.routes.js';
+//avec comonjs, const post=require('./routes/post.routes.js)
 const port=3001;
 const app= express();
 
-app.get('/post', (req, res)=>{
-    res.end('Welcome to you');
-})
+
+//
+app.use(express.json())
+app.use('/', postRoute )
+app.use('/post', postRoute )
+
+
+
+
+// app.get('/', (req, res)=>{
+//     res.json({
+//         message:"page d'accueil",
+//     })
+// })
+
+// app.get('/post', (req, res)=>{
+//     // res.end('Welcome to you');
+//     res.json({
+//         id:1,
+//         title:"Mon premier post",
+//         content:"Contenu du premier post"
+//     });
+// })
+
 app.listen(port, ()=>{
     console.log(`serveur creer sur le port ${port} avec succes`);
 })
